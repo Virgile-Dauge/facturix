a23fe14a8c35663232c81328f882d5c80065362a
Virgile <virgile.dauge@pm.me>
2024-09-20 18:25:42 +0200

    ajout logs et validation

--- sandbox/facturix.py
+++ sandbox/facturix.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 import argparse
 import pandas as pd
+import pandas.testing as pdt
 
 from pathlib import Path
 from facturx import generate_from_file
 
 from extract_from_pdf import extraire_num_facture
 from populate_xml import gen_xmls
+from validate_xml import validate_xml_with_xsd, validate_xml_with_schematron
+
+import logging
+
+# Configurer le logger
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.getLogger('factur-x').setLevel(logging.WARN)
+
+
+logger = logging.getLogger(__name__)
+
 
 def main():
     parser = argparse.ArgumentParser(description="Lister les fichiers PDF dans un dossier de données.")
@@ -43,39 +55,46 @@ def main():
         
         # On s'assure qu'on a bien récup tous les nums de facture
         assert len(pdfs) == len([n for n in num_fact if n is not None])
-        print(num_fact)
         
         # Créer une DataFrame avec les couples pdfs et num_fact
         link_df = pd.DataFrame({
-            'pdf': [str(f) for f in pdfs],
+            'pdf': pdfs,
             'num_facture': num_fact
         })
         
         # Enregistrer la DataFrame dans un fichier CSV
         link_df.to_csv(link_csv, index=False)
-        
         print(f"Les couples pdfs et num_fact ont été enregistrés dans {link_csv}")
-    else:
-        # Charger le fichier CSV existant
-        link_df = pd.read_csv(link_csv)
-        print(f"Le fichier {link_csv} a été chargé.")
-    
+
+    # Charger le fichier CSV
+    link_df = pd.read_csv(link_csv)
+    print(f"Le fichier {link_csv} a été chargé.")
+
     # On charge le fichier de données
     df = pd.read_csv(args.input_csv)
-    print(df.head())
     
     # Convertir les colonnes en chaînes de caractères avant la fusion
     df['BT-1'] = df['BT-1'].astype(str)
     link_df['num_facture'] = link_df['num_facture'].astype(str)
     # Fusionner les DataFrames df et link_df sur la colonne 'num_facture'
-    df_merged = df.merge(link_df[['pdf']], left_on='BT-1', right_on=link_df['num_facture'], how='left')
+    df_merged = df.merge(link_df, left_on='BT-1', right_on='num_facture', how='left').drop(columns=['num_facture'])
 
     # Afficher les premières lignes du DataFrame fusionné pour vérification
     print(df_merged.head())
 
     xml_template = Path('sandbox/minimum_template.xml')  # Chemin vers le modèle XML
     to_embed = gen_xmls(df_merged, xml_template, output_dir)
-    print(to_embed)
+    
+    schematron_file = Path('validators/FACTUR-X_MINIMUM_custom.sch')
+    xsd_file = Path('validators/FACTUR-X_MINIMUM.xsd')  # Remplacer par le chemin réel du XSD
+
+    produced_xml = output_dir.glob('*.xml')
+    
+    invalid = [x for x in produced_xml if not (validate_xml_with_xsd(x, xsd_file) and validate_xml_with_schematron(x, schematron_file))]
+
+    if invalid:
+        logger.error(f"Les fichiers XML suivants ne sont pas valides : {invalid}")
+    
     
     for p, x in to_embed:
         
